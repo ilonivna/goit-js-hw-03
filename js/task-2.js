b5a016a14cc50ccfc9764c18dadc68d5134a9563
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-01-26 03:02:04 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,8 @@
 function makeArray(firstArray, secondArray, maxLength) {
     const thirdArray = firstArray.concat(secondArray);
-    if (thirdArray.length > maxLength) { return thirdArray.slice(0, maxLength); }
-    else {
+    if (thirdArray.length > maxLength) { 
+        return thirdArray.slice(0, maxLength); 
+    } else {
         return thirdArray;
     }
 }
